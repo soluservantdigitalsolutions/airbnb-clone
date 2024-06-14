b7cfcfe3b65f00b1b8bcf9450d4fabe686c36eb5
asantesylvesterkwame <intelligentson17@gmail.com>
2024-06-14 10:17:50 +0000

    setup the the AUTH UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
+import { Toaster } from "react-hot-toast";
+import RegisterModal from "./components/Modals/RegisterModal";
 import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
     <div>
+      <Toaster />
+      <RegisterModal />
       <Navbar />
-      
     </div>
   );
 };
---
+++ client/src/utils/index.ts
@@ -0,0 +1,7 @@
+export const jsonResponse = (data: any) => {
+  return new Response(JSON.stringify(data), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
